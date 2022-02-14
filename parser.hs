51b89f482e992e8536c6ac0d5890ab56acd416e7
Noah Brooks <noahebrooks@protonmail.ch>
2022-02-13 21:15:38 -0500

    can eval s-expr

--- parser.hs
+++ parser.hs
@@ -11,17 +11,16 @@ data LispVal = Atom String
 			       | Bool Bool
 
 main :: IO ()
-main = do args <- getArgs
-          putStrLn (readExpr (args !! 0))
+main = getArgs >>= putStrLn . showVal . eval . readExpr . (!! 0)
 
 
 symbol :: Parser Char 
 symbol = oneOf "!$%&|*+-/:<=?>@^_~#"
 
-readExpr :: String -> String
+readExpr :: String -> LispVal
 readExpr input = case parse parseExpr "lisp" input of
-								 Left err -> "No match: " ++ show err
-								 Right val -> showVal val
+								 Left err -> String $ "No match: " ++ show err
+								 Right val -> val
 
 spaces :: Parser ()
 spaces = skipMany1 space
@@ -88,3 +87,34 @@ showVal (DottedList head tail) = "(" ++ unwordsList head ++ " . " ++ showVal tai
 
 unwordsList :: [LispVal] -> String
 unwordsList = unwords . map showVal
+
+eval :: LispVal -> LispVal
+eval val@(String _) = val
+eval val@(Number _) = val
+eval val@(Bool _) = val
+eval (List [Atom "quote", val]) = val
+eval (List (Atom func : args)) = apply func $ map eval args
+
+apply :: String -> [LispVal] -> LispVal
+apply func args = maybe (Bool False) ($ args) $ lookup func primitives
+
+primitives :: [(String, [LispVal] -> LispVal)]
+primitives = [("+", numericBinop (+)),
+              ("-", numericBinop (-)),
+              ("*", numericBinop (*)),
+              ("/", numericBinop div),
+              ("mod", numericBinop mod),
+              ("quotient", numericBinop quot),
+              ("remainder", numericBinop rem)]
+
+numericBinop :: (Integer -> Integer -> Integer) -> [LispVal] -> LispVal
+numericBinop op params = Number $ foldl1 op $ map unpackNum params
+
+unpackNum :: LispVal -> Integer
+unpackNum (Number n) = n
+unpackNum (String n) = let parsed = reads n in
+                           if null parsed
+                              then 0
+                              else fst $ parsed !! 0
+unpackNum (List [n]) = unpackNum n
+unpackNum _ = 0
